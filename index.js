0a64f6506ad6498ad350c9bab3b68155bfb3c424
Heidi Sheng <heidisheng@MacBook-Air.local>
2020-09-21 13:09:57 -0400

    changed  greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 //if there is an environment variable defined by HEROKU, assign this variable
