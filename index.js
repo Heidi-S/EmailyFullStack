6294a61adb2d6e86f294cc4d18742fcf53992be6
Heidi Sheng <heidisheng@MacBook-Air.local>
2020-09-20 23:59:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+//if there is an environment variable defined by HEROKU, assign this variable
+//to port, otherwise use default port 5000 (for development environment)
+//Use all capital letters for PORT because this value seldom changes.
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+//localhost:5000
