f409b7488e3094f1287745e53266698144bd933c
Heidi Sheng <heidisheng@MacBook-Air.local>
2020-09-22 15:10:42 -0400

    finished auth

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+//maxAge: how long this cookie can exist in browser before automatically
+//expired. unit in millisecond.
+//keys: used to encrypt our cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 //if there is an environment variable defined by HEROKU, assign this variable
 //to port, otherwise use default port 5000 (for development environment)
